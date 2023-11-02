0afbc42a5fa9fee7c87ea6fa92224ad535c14167
Hannes Bröhl <hannes.broehl@icloud.com>
2023-11-02 08:49:04 +0100

    finished + add time-function

--- From-static-HTML-to-a-Vue-Instance/src/script.js
+++ From-static-HTML-to-a-Vue-Instance/src/script.js
@@ -3,8 +3,18 @@
 const app = Vue.createApp({
     data(){
         return {
-            userName: 'Hannes B.'
-            //time: date.toLocaleString()
+            userName: 'Hannes B.',
+            time: new Date().toLocaleString()
+        }
+    },
+    mounted() {
+        this.updateTime()
+    },
+    methods: {
+        updateTime() {
+            setInterval(() => {
+                this.time = new Date().toLocaleString('en-us');
+            }, 1000);
         }
     }
 }).mount('#card')
