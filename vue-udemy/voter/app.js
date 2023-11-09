0b0dbf87da2a7e30ba5ad3b4a0a953c548ecd1c9
Hannes Bröhl <hannes.broehl@icloud.com>
2023-11-09 14:19:52 +0100

    bis inkl. 29. Zwischenfazit ....

--- vue-udemy/voter/app.js
+++ vue-udemy/voter/app.js
@@ -5,7 +5,44 @@ const app = Vue.createApp({
   data: function () {
     return {
       submissions: submissions, //aus seed.js
+      totalVotes: 0,
     };
+  },
+  // computed: {
+  //   totalVotes() {
+  //     return this.submissions.reduce((totalVotes, submission) => {
+  //       return totalVotes + submission.votes
+  //     }, 0);
+  //   },
+  // },
+  methods: {
+    //    upvote: function() {}, ==> keine Arrow-Functions nutzen!!!
+    upvote(event) {
+      this.submissions[0].votes++;
+    },
+    logConsole(text) {
+      // console.log(text);
+    },
+  },
+  watch: {
+    // submissions(newValue, oldValue) {
+    //   console.log(newValue);
+    //   console.log(oldValue);
+    // },
+
+  submissions: {
+    handler(newValue, oldValue) {
+      this.totalVotes = this.submissions.reduce((totalVotes, submissions) => {
+        return totalVotes + submissions.votes;
+      }, 0);
+    },
+    deep: true,
+    immediate: true,
+  },
+  totalVotes(newValue, oldValue) {
+    console.log(newValue);
+      console.log(oldValue);
+   },
   },
 });
 
