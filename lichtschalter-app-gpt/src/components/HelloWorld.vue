8be831a0c34561550a8aa58c4a4e3ad84293c580
Hannes Bröhl <hannes.broehl@icloud.com>
2023-11-13 15:50:48 +0100

    Light-Switch with vue (GPT)

---
+++ lichtschalter-app-gpt/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Lichtschalter-App</h1>
+    <p>Beleuchtung: {{ status }}</p>
+    <button @click="toggleLight">Lichtschalter</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLightOn: false,
+    };
+  },
+  computed: {
+    status() {
+      return this.isLightOn ? 'An' : 'Aus';
+    },
+  },
+  methods: {
+    toggleLight() {
+      this.isLightOn = !this.isLightOn;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Füge hier dein CSS hinzu, falls du möchtest */
+</style>
---
+++ lichtschalter-app-gpt/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style scoped>
+/* Füge hier dein CSS hinzu, falls du möchtest */
+</style>
